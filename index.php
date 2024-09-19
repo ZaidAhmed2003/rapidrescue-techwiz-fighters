ba9e95aee47100484f38350143c1427ba218057c
Zaid Ahmed <zaidahmed2345@gmail.com>
2024-09-19 15:35:11 +0500

    chah

--- index.php
+++ index.php
@@ -3,8 +3,12 @@ require "includes/header.php";
 require "includes/navbar.php"
 ?>
 
+<?php
+require "includes/hero.php";
+require "includes/aboutus.php"
+?>
 // code goes here
 
 <?php
-require "includes/header.php"
+require "includes/footer.php"
 ?>
