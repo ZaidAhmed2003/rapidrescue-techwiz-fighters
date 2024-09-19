614691151097d88501921a33d11531d6cb06e2ab
Zaid Ahmed <zaidahmed2345@gmail.com>
2024-09-19 12:02:12 +0500

    assets

---
+++ includes/navbar.php
@@ -0,0 +1,10 @@
+<nav class="flex justify-between py-5">
+    <a href="">Logo</a>
+    <ul class="flex font-semibold gap-5">
+        <li>Home</li>
+        <li>Services</li>
+        <li>Contact</li>
+        <li>Request An Ambulance</li>
+        <li>Login</li>
+    </ul>
+</nav>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
-require "includes/header.php"
+require "includes/header.php";
+require "includes/navbar.php"
 ?>
 
 // code goes here
